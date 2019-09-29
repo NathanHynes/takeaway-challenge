860b92b4a497e38bcc74f600943a2726dc6c3884
Nathan Hynes <nathanmail@hotmail.co.uk>
2019-09-29 19:02:20 +0100

    fixed issue with order tests.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -4,7 +4,7 @@ describe Order do
   let(:order) { Order.new(menu) }
   let(:menu) { double :menu, menu: dishes, add_item: '', item_on_menu?: '' }
   let(:dishes) { { pizza: 3, drink: 4, pie: 5 } }
-  let(:printed_order) { "Current order:\n3 Korma\n4 Pizza\n" }
+  let(:printed_order) { "Current order:\n3 Drink\n4 Pizza\n" }
   let(:dish) { :pizza }
 
   it "initialises with an empty order" do
@@ -16,7 +16,7 @@ describe Order do
   end
 
   describe "#add_to_basket" do
-    let(:item) { :Korma }
+    let(:item) { 'Korma' }
     let(:quantity) { 3 }
     it { is_expected.to respond_to(:add_to_basket).with(2).arguments }
 
@@ -26,14 +26,14 @@ describe Order do
     end
 
     it "adds item to order" do
-      order.add_to_basket(item, quantity)
-      expect(order.current_order.flat_map(&:keys)).to include(item.to_sym)
+      order.add_to_basket(:pizza, 4)
+      expect(order.current_order.flat_map(&:keys)).to include(:Pizza)
     end
   end
 
   describe '#print_order' do
     before do
-      order.add_to_basket(:korma, 3)
+      order.add_to_basket(:drink, 3)
       order.add_to_basket(:pizza, 4)
     end
     it "prints order to standard output" do
